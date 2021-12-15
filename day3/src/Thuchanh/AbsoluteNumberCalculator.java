3cae2bcb6d5c2ea60996c5193fe3a66ba2c3288f
RicardoLolo <loloringo9999@gmail.com>
2021-12-15 11:40:02 +0700

    secondweek

---
+++ day3/src/Thuchanh/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package Thuchanh;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
---
+++ day3/test/Baitap/NextDayCalculatorTest.java
@@ -0,0 +1,18 @@
+package Baitap;
+
+import org.junit.jupiter.api.Test;
+
+class NextDayCalculatorTest {
+
+    @Test
+    void nextDay() {
+    }
+
+    @Test
+    void isLeapYear() {
+    }
+
+    @Test
+    void getDaysOfMonth() {
+    }
+}
