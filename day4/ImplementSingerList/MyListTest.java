2f2d135cf067ad74e22444d2d3df76916e207fb2
RicardoLolo <loloringo9999@gmail.com>
2021-12-16 14:12:54 +0700

    secondweek

---
+++ day4/ImplementSingerList/MyListTest.java
@@ -0,0 +1,18 @@
+package ImplementSingerList;
+
+public class MyListTest {
+
+    @org.junit.jupiter.api.Test
+    public static void main(String[] args) {
+        MyList<Integer> listInterger = new MyList<>();
+        listInterger.add(1);
+        listInterger.add(2);
+        listInterger.add(3);
+        listInterger.add(4);
+        listInterger.add(5);
+
+        System.out.println("element 4:" + listInterger.get(4));
+        System.out.println("element 1:" + listInterger.get(1));
+        System.out.println("element 2:" + listInterger.get(2));
+    }
+}
